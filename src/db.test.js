71232f0e55e465a61b93615b4ac8b776265f5945
juliasut <juliasutygina@gmail.com>
2023-04-12 00:02:57 -0600

    reset db between tests

--- src/db.test.js
+++ src/db.test.js
@@ -7,6 +7,9 @@ import {
 } from './test-helpers';
 
 describe('getUserByUsername', () => {
+  afterEach('reset the database', async () => {
+    await resetDatabase();
+  });
   it('should get the correct user name from the database given a username', async () => {
     const fakeData = [
       {
@@ -25,7 +28,6 @@ describe('getUserByUsername', () => {
 
     const actual = await getUserByUsername('abc');
     const finalDBState = await getDatabaseData('users');
-    await resetDatabase();
 
     const expected = {
       id: '123',
